c3193c11edb054fdee019edd0ac238f5ee00c557
Johnny Lin <johnny.lin.prime@gmail.com>
2020-06-30 19:23:43 -0400

    Fix gitignore to include dbsetup code, but not the compiled executable file.

---
+++ backend/cmd/dbsetup/dbsetup.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/jinzhu/gorm"
+  	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func main() {
+	godotenv
+	// fmt.Println("Hello world!")
+	for true {
+	}
+}
