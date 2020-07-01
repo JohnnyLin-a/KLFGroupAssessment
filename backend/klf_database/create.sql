92fbb467cfb81689bc445800218e2c58ecd03ca3
Johnny Lin <johnny.lin.prime@gmail.com>
2020-07-01 15:24:20 -0400

    Modify wording for create statements to match postgres dialect

--- backend/klf_database/create.sql
+++ backend/klf_database/create.sql
@@ -1,10 +1,10 @@
-CREATE TABLE user(
-    id SERIAL,
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
     name TEXT
 );
 
-CREATE TABLE activity(
-    id SERIAL,
+CREATE TABLE activities(
+    id SERIAL PRIMARY KEY,
     name TEXT
 );
 
@@ -12,6 +12,6 @@ CREATE TABLE user_activity(
     activity_id INT,
     user_id INT,
     occurrence timestamp,
-    FOREIGN KEY(activity_id) REFERENCES activity(id),
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(activity_id) REFERENCES activities(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
