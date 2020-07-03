f27c49fcce6121bccb5161046fc134827d0c3916
Johnny Lin <johnny.lin.prime@gmail.com>
2020-07-03 03:11:30 -0400

    Implement redux persist

--- frontend/src/Store/Store.js
+++ frontend/src/Store/Store.js
@@ -1,12 +1,18 @@
-import rootReducer from '../Reducers';
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-/**
- * Redux store
- */
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+import rootReducer from '../Reducers';
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+    let store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    let persistor = persistStore(store);
+    return { store, persistor };
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './Store/Store';
+import { PersistGate } from 'redux-persist/integration/react'
 
-
+const reduxstore = store()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={reduxstore.store}>
+            <PersistGate loading={null} persistor={reduxstore.persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
