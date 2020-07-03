ef7eab7535924f6d98f114d9ed86d7680b8a7e28
Johnny Lin <johnny.lin.prime@gmail.com>
2020-07-03 16:03:29 -0400

    Make body fit in a bootstrap container

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -6,6 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { logout } from '../Actions/AuthActions'
+import Container from 'react-bootstrap/Container'
 class Header extends Component {
     state = {
         navExpanded: false
@@ -26,32 +27,35 @@ class Header extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg" onToggle={this.setNavExpanded} expanded={this.state.navExpanded}>
-                <Navbar.Brand><Link to="/" className="m-3 text-dark" style={{ textDecoration: 'none' }}>AccessLink</Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Link to="/" className="m-3 text-dark" style={{ textDecoration: 'none' }}>Home</Link>
-                        <Link to="/about" className="m-3 text-dark" style={{ textDecoration: 'none' }}>About</Link>
-                    </Nav>
-                    {this.props.user.token ?
-                        <NavDropdown alignRight title={`Welcome, ${this.props.user.name}`} id="basic-nav-dropdown">
-                            <NavDropdown.Item>
-                                <Link className="nav-link" to="/profile">Profile</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item onClick={() => { this.props.dispatch(logout()) }}>Logout</NavDropdown.Item>
-                        </NavDropdown>
-                        :
-                        <>
-                            <Button className="mr-3" onClick={() => { this.closeNav(); this.props.history.push("/login") }}>
-                                Login
-                            </Button>
-                            <Button onClick={() => { this.closeNav(); this.props.history.push("/register") }}>
-                                Register
-                            </Button>
-                        </>
-                    }
-                </Navbar.Collapse>
+                <Container>
+                    <Navbar.Brand><Link to="/" className="m-3 text-dark" style={{ textDecoration: 'none' }}>AccessLink</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Link to="/" className="m-3 text-dark" style={{ textDecoration: 'none' }}>Home</Link>
+                            <Link to="/about" className="m-3 text-dark" style={{ textDecoration: 'none' }}>About</Link>
+                            <Link to="/contact-us" className="m-3 text-dark" style={{ textDecoration: 'none' }}>Contact us</Link>
+                        </Nav>
+                        {this.props.user.token ?
+                            <NavDropdown alignRight title={`Welcome, ${this.props.user.name}`} id="basic-nav-dropdown">
+                                <NavDropdown.Item>
+                                    <Link className="nav-link" to="/profile">Profile</Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={() => { this.props.dispatch(logout()) }}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                            :
+                            <>
+                                <Button className="mr-3" onClick={() => { this.closeNav(); this.props.history.push("/login") }}>
+                                    Login
+                                </Button>
+                                <Button onClick={() => { this.closeNav(); this.props.history.push("/register") }}>
+                                    Register
+                                </Button>
+                            </>
+                        }
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         )
     }
