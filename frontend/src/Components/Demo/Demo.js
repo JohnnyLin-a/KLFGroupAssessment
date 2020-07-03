1f264fd23dc4728d3a2d0a75b21fdb9ce13af554
Johnny Lin <johnny.lin.prime@gmail.com>
2020-07-03 19:19:56 -0400

    Add demo template

--- frontend/src/Components/Demo/Demo.js
+++ frontend/src/Components/Demo/Demo.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Table from 'react-bootstrap/Table';
+import Spinner from 'react-bootstrap/Spinner';
 
 class Demo extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            tableData: true,
+        }
+    }
 
     render() {
         return (
-            <div>
-                <p>
-                    Demo
-                </p>
-            </div>
+            <Container className="p-5">
+                <Jumbotron>
+                    <h1 className="text-center">AccessLink demo</h1>
+                    <h6 className="text-center p-3">
+                        Displaying data from October 2019
+                    </h6>
+                    <hr />
+                    {this.state.tableData ?
+                        <Table responsive>
+                            <thead>
+                                <tr>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                                <tr>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                                <tr>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        :
+                        <div className="text-center">
+                            <h5 className="p-3">Loading</h5>
+                            <Spinner animation="border" role="status">
+                                <span className="sr-only m-3" >Loading...</span>
+                            </Spinner>
+                        </div>
+
+
+                    }
+                </Jumbotron>
+            </Container>
         )
     }
 }
