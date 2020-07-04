ad501f8be35a79a7d27e24ddfd366ea55d9242a4
Johnny Lin <johnny.lin.prime@gmail.com>
2020-07-03 20:35:23 -0400

    Display report for October 2019 on frontend

--- frontend/src/Components/Demo/Demo.js
+++ frontend/src/Components/Demo/Demo.js
@@ -3,19 +3,53 @@ import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Table from 'react-bootstrap/Table';
 import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 
 class Demo extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            tableData: true,
+            tableData: null,
+            errorMessage: '',
         }
+    }
+
+    componentDidMount = () => {
+        fetch(`${process.env.REACT_APP_API_URL}/demo`, {
+            method: 'get',
+        }).then(response => {
+            switch (response.status) {
+                case 200:
+                    const jsonResponse = response.json();
+                    return jsonResponse;
+                case 500:
+                    this.setState({ errorMessage: "Internal server error" })
+                    break;
+                default:
+                    this.setState({ errorMessage: "Network error. Please try again." })
+                    break;
+            }
+        }).then(data => {
+            if (typeof data !== 'undefined') {
+                this.setState({ tableData: data }, () => {
+                    console.log("data", data)
+                })
+            }
+        }).catch(error => {
+            console.error(error);
+        });
     }
 
     render() {
         return (
             <Container className="p-5">
+                {this.state.errorMessage &&
+                    <Alert variant="danger">
+                        {this.state.errorMessage}
+                    </Alert>
+                }
+
                 <Jumbotron>
                     <h1 className="text-center">AccessLink demo</h1>
                     <h6 className="text-center p-3">
@@ -26,35 +60,19 @@ class Demo extends Component {
                         <Table responsive>
                             <thead>
                                 <tr>
-                                    <th>Table heading</th>
-                                    <th>Table heading</th>
-                                    <th>Table heading</th>
-                                    <th>Table heading</th>
-                                    <th>Table heading</th>
+                                    {this.state.tableData.th.map(th =>
+                                        <th>{th}</th>
+                                    )}
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                </tr>
-                                <tr>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                </tr>
-                                <tr>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                    <td>Table cell</td>
-                                </tr>
+                                {this.state.tableData.td.map(row =>
+                                    <tr>
+                                        {row.map(cellData =>
+                                            <td>{cellData}</td>
+                                        )}
+                                    </tr>
+                                )}
                             </tbody>
                         </Table>
                         :
